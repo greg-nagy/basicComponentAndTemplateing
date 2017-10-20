981c585e142cdd193061b2be9291adb4e43f7cc4
puffy <nagy.gergo@3pixel.hu>
2017-10-20 18:36:49 +0200

    add new event - esemeny nev es max id lekezelve
    reduce kulon pl-ben konstruktorban kommentben

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class EventModel {
-  id: number;
-  name: string;
-}
+import { EventModel } from './event-model';
 
 @Component({
   selector: 'app-listdemo',
@@ -11,24 +7,45 @@ export class EventModel {
   styleUrls: ['./listdemo.component.css']
 })
 export class ListdemoComponent {
+  // XSS challange, image tag src, miau szoveggel szeretnek egy alert-et
   events: EventModel[];
 
   constructor() {
     this.events = [
       {
         id: 1,
-        name: 'sziget'
+        name: 'sziget',
+        pic: 'http://sziget.hu/templates/sziget2017-2/images/logo.png'
+      },
+      {
+        id: 33,
+        name: 'fezen',
+        pic: 'http://www.fezenklub.hu/_user/browser/Image/2016_17/FEZEN_2017_WHITE%20(1).jpg'
       },
       {
         id: 2,
-        name: 'fezen'
-      },
-      {
-        id: 3,
-        name: 'rockmaraton'
+        name: 'rockmaraton',
+        pic: 'http://hammerworld.hu/images/upload/01arokka17.jpg?20110201-164743'
       }
     ];
     console.log('megjott a listdemo cmp');
+    // const puf = this.events.reduce((x: EventModel, y: EventModel) => {
+    //   // if (x.id > y.id) {
+    //   //   return x;
+    //   // } else {
+    //   //   return y;
+    //   // }
+    //   //
+    //   // (logikai vizsgalat) ? haigaz : hahamis;
+    //   return x.id > y.id ? x : y;
+    // }).id;
+    // console.log(puf);
+  }
+
+  add(newEventNameInput: HTMLInputElement) {
+    const maxId = this.events.reduce((x, y) => x.id > y.id ? x : y).id;
+    this.events = [...this.events, new EventModel(maxId + 1, newEventNameInput.value)];
+    newEventNameInput.value = '';
   }
 
   delete(id: number) {
